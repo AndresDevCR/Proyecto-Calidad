f5ba79b7a56958a8c43434ac399059582204e031
Nalle12 <nallelyalfaro02@gmail.com>
2022-08-07 23:45:07 -0700

    Test Nallely Finalizado

--- src/test/java/TestNallely/TestNallely.java
+++ src/test/java/TestNallely/TestNallely.java
@@ -1,5 +1,6 @@
 package TestNallely;
 
+import org.junit.After;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -29,13 +30,14 @@ public class TestNallely {
     @Test
     public void test() {
         //click sobre el search field
-      driver.findElement(By.xpath("//*[@id=\"content\"]/div[2]/div[4]/div/div[1]/a/img")).click();
-      driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
-      driver.findElement(By.xpath("//*[@id=\"input-option226\"]")).click();
-      driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
-      driver.findElement(By.xpath("//*[@id=\"input-option226\"]/option[2]")).click();
+        elemento= driver.findElement(By.xpath("//*[@id=\"search\"]/input"));
+        driver.findElement(By.xpath("//*[@id=\"content\"]/div[2]/div[4]/div/div[1]/a/img")).click();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
-      driver.findElement(By.xpath("//*[@id=\"button-cart\"]")).click();
+        driver.findElement(By.xpath("//*[@id=\"input-option226\"]")).click();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
+        driver.findElement(By.xpath("//*[@id=\"input-option226\"]/option[2]")).click();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
+        driver.findElement(By.xpath("//*[@id=\"button-cart\"]")).click();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
         driver.findElement(By.xpath("//*[@id=\"cart\"]/button")).click();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
@@ -47,19 +49,47 @@ public class TestNallely {
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
         driver.findElement(By.xpath("//*[@id=\"button-account\"]")).click();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
-        driver.findElement(By.xpath("//*[@id=\"input-payment-firstname\"]")).click();
-        elemento.sendKeys("Nallely");
+        driver.findElement(By.xpath("//*[@id=\"input-payment-firstname\"]")).sendKeys("Nallely");
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
-        driver.findElement(By.xpath("//*[@id=\"input-payment-lastname\"]")).click();
-        elemento.sendKeys("Alfaro");
+        driver.findElement(By.xpath("//*[@id=\"input-payment-lastname\"]")).sendKeys("Alfaro");
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
-        driver.findElement(By.xpath("//*[@id=\"input-payment-email\"]")).click();
-        elemento.sendKeys("nallelyalfaro02@mail.com");
+        driver.findElement(By.xpath("//*[@id=\"input-payment-email\"]")).sendKeys("nallelyalfaro02@mail.com");
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
-
-
-
+        driver.findElement(By.xpath("//*[@id=\"input-payment-telephone\"]")).sendKeys("123456789");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
+        driver.findElement(By.xpath("//*[@id=\"input-payment-address-1\"]")).sendKeys("Calle de la paz");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
+        driver.findElement(By.xpath("//*[@id=\"input-payment-city\"]")).sendKeys("Madrid");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
+        driver.findElement(By.xpath("//*[@id=\"input-payment-postcode\"]")).sendKeys("28001");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
+        driver.findElement(By.xpath("//*[@id=\"input-payment-country\"]")).sendKeys("Spain");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
+        driver.findElement(By.xpath("//*[@id=\"input-payment-zone\"]")).sendKeys("Madrid");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
+        driver.findElement(By.xpath("//*[@id=\"input-payment-zone\"]")).click();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
+        driver.findElement(By.xpath("//*[@id=\"input-payment-zone\"]/option[6]")).click();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
+        driver.findElement(By.xpath("//*[@id=\"button-guest\"]")).click();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
+        driver.findElement(By.xpath("//*[@id=\"collapse-shipping-method\"]/div/p[4]/textarea")).click();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
+        driver.findElement(By.xpath("//*[@id=\"collapse-shipping-method\"]/div/p[4]/textarea")).sendKeys("Test");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
+        driver.findElement(By.xpath("//*[@id=\"button-shipping-method\"]")).click();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
+        driver.findElement(By.cssSelector("#collapse-payment-method > div > div.buttons > div > input[type=checkbox]:nth-child(2)")).click();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
+        driver.findElement(By.xpath("//*[@id=\"button-payment-method\"]")).click();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
+        driver.findElement(By.xpath("//*[@id=\"button-confirm\"]")).click();
     }
 
+    @After
+    public void tearDown() {
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.MINUTES);
+        driver.quit();
+    }
 
 }
